b7c5300f89ee501bb994bd21008155356813010b
Link1986 <ali.ghoudi@gmail.com>
2018-06-04 15:01:47 +0200

    mise à jour du routage

--- src/OC/ProjectBundle/Controller/AdvertController.php
+++ src/OC/ProjectBundle/Controller/AdvertController.php
@@ -2,113 +2,141 @@
 
 namespace OC\ProjectBundle\Controller;
 
-use OC\ProjectBundle\Entity\Advert;
-use OC\ProjectBundle\Entity\Image;
-use OC\ProjectBundle\Entity\Application;
+use OC\ProjectBundle\Entity\Todo;
 use OC\ProjectBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use OC\ProjectBundle\Form\UserType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AdvertController extends Controller
 {
     public function indexAction()
     {
-
         return $this->render('OCProjectBundle:Advert:index.html.twig');
     }
 
-    public function viewAction(Request $request)
+    public function memberAction()
     {
-        $user = new User();
-        $form   = $this->get('form.factory')->create(UserType::class, $user);
+        return $this->render('OCProjectBundle:Advert:member.html.twig');
+    }
 
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
-
-            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
-
-            return $this->redirectToRoute('oc_project_home');
-        }
-
-        return $this->render('OCProjectBundle:Advert:view.html.twig', array(
-            'form' => $form->createView(),
-        ));
-
+    public function viewAction($id)
+    {
+        return $this->render('OCProjectBundle:Advert:view.html.twig', array('id' => $id));
     }
 
     public function addAction(Request $request)
     {
-        // Création de l'entité Advert
-        $advert = new Advert();
-        $advert->setTitle('Recherche développeur Symfony.');
-        $advert->setAuthor('Alexandre');
-        $advert->setContent("Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…");
 
-        // Création d'une première candidature
-        $application1 = new Application();
-        $application1->setAuthor('Marine');
-        $application1->setContent("J'ai toutes les qualités requises.");
+        $todo = new Todo();
 
-        // Création d'une deuxième candidature par exemple
-        $application2 = new Application();
-        $application2->setAuthor('Pierre');
-        $application2->setContent("Je suis très motivé.");
+        $form = $this->get('form.factory')->createBuilder(FormType::class, $todo)
+            ->add('list',      TextType::class)
+            ->add('ajouter',      SubmitType::class)
+            ->getForm()
+        ;
 
-        // On lie les candidatures à l'annonce
-        $application1->setAdvert($advert);
-        $application2->setAdvert($advert);
+        // Si la requête est en POST
+        if ($request->isMethod('POST')) {
+            // On fait le lien Requête <-> Formulaire
+            // À partir de maintenant, la variable $advert contient les valeurs entrées dans le formulaire par le visiteur
+            $form->handleRequest($request);
 
-        // On récupère l'EntityManager
-        $em = $this->getDoctrine()->getManager();
+            // On vérifie que les valeurs entrées sont correctes
+            // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+            if ($form->isValid()) {
+                // On enregistre notre objet $advert dans la base de données, par exemple
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($todo);
+                $em->flush();
 
-        // Étape 1 : On « persiste » l'entité
-        $em->persist($advert);
+                $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
 
-        // Étape 1 ter : pour cette relation pas de cascade lorsqu'on persiste Advert, car la relation est
-        // définie dans l'entité Application et non Advert. On doit donc tout persister à la main ici.
-        $em->persist($application1);
-        $em->persist($application2);
 
-        // Étape 2 : On « flush » tout ce qui a été persisté avant
+
+                // On redirige vers la page de visualisation de l'annonce nouvellement créée
+                return $this->redirectToRoute('oc_project_add', array('id' => $todo->getId()));
+            }
+        }
+
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('OCProjectBundle:Todo')
+        ;
+
+        $listAdverts = $repository->myFindAll();
+
+        // On passe la méthode createView() du formulaire à la vue
+        // afin qu'elle puisse afficher le formulaire toute seule
+        return $this->render('OCProjectBundle:Advert:add.html.twig', array(
+            'form' => $form->createView(),
+            'listAdverts' => $listAdverts
+        ));
+    }
+
+    public function editAction(Request $request, Todo $todo)
+    {
+        $form = $this->get('form.factory')->createBuilder(FormType::class, $todo)
+            ->add('list',      TextType::class)
+            ->add('ajouter',      SubmitType::class)
+            ->getForm()
+        ;
+
+        // Si la requête est en POST
+        if ($request->isMethod('POST')) {
+            // On fait le lien Requête <-> Formulaire
+            // À partir de maintenant, la variable $advert contient les valeurs entrées dans le formulaire par le visiteur
+            $form->handleRequest($request);
+
+            // On vérifie que les valeurs entrées sont correctes
+            // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+            if ($form->isValid()) {
+                // On enregistre notre objet $advert dans la base de données, par exemple
+                $em = $this->getDoctrine()->getManager();
+
+                $em->flush();
+
+                $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+
+
+
+            }
+        }
+
+
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('OCProjectBundle:Todo')
+        ;
+
+        $listAdverts = $repository->myFindAll();
+
+        // On passe la méthode createView() du formulaire à la vue
+        // afin qu'elle puisse afficher le formulaire toute seule
+        return $this->render('OCProjectBundle:Advert:edit.html.twig', array(
+            'form' => $form->createView(),
+            'listAdverts' => $listAdverts
+        ));
+
+
+    }
+
+    public function deleteAction(Todo $todo)
+    {
+        $em = $this
+            ->getDoctrine()
+            ->getManager();
+        $em->remove($todo);
         $em->flush();
 
-        // Reste de la méthode qu'on avait déjà écrit
-        if ($request->isMethod('POST')) {
-            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
-
-            // Puis on redirige vers la page de visualisation de cettte annonce
-            return $this->redirectToRoute('oc_project_view', array('id' => $advert->getId()));
-        }
-
-        // Si on n'est pas en POST, alors on affiche le formulaire
-        return $this->render('OCProjectBundle:Advert:add.html.twig', array('advert' => $advert));
-        
-    }
-
-    public function editAction($id, Request $request)
-    {
-        // Ici, on récupérera l'annonce correspondante à $id
-
-        // Même mécanisme que pour l'ajout
-        if ($request->isMethod('POST')) {
-            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
-
-            return $this->redirectToRoute('oc_project_view', array('id' => 5));
-        }
-
-        return $this->render('OCProjectBundle:Advert:edit.html.twig');
-    }
-
-    public function deleteAction($id)
-    {
-        // Ici, on récupérera l'annonce correspondant à $id
-
-        // Ici, on gérera la suppression de l'annonce en question
-
-        return $this->render('OCProjectBundle:Advert:delete.html.twig');
+        return new Response('élément supprimé');
     }
 }
